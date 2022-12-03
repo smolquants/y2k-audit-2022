c5d6bb4dedb1dc74da281d3dc970918e47eea3f2
fmrmf <m@fmrmf.xyz>
2022-12-02 20:06:08 -0500

    add mock liquidity to curve pool for manip

--- scripts/oracle/curve_manipulation.py
+++ scripts/oracle/curve_manipulation.py
@@ -8,6 +8,10 @@ def mim_curve_pool() -> ContractInstance:
     return Contract("0x5a6A4D54456819380173272A5E8E9B9904BdF41B")
 
 
+def crv3_curve_pool() -> ContractInstance:
+    return Contract("0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7")
+
+
 def deploy_mock_token(
     name: str,
     symbol: str,
@@ -40,6 +44,43 @@ def deploy_mock_pool(
     )
 
 
+def mint_liquidity_to_mock_pool(
+    actual_pool: ContractInstance,
+    base_pool: ContractInstance,
+    mock_pool: ContractInstance,
+    mock_token0: ContractInstance,
+    mock_token1: ContractInstance,
+    acc: AccountAPI,
+) -> int:
+    """
+    Mints liquidity to the mock pool replicating liquidity
+    in the actual metapool.
+
+    Assumes coin0 is MIM and coin1 is 3crv in the actual pool. Also,
+    takes all coins in the base 3crv pool to be ~ 1 USD.
+    """
+    # calculate MIM balance in actual pool
+    amount0 = actual_pool.balances(0)
+
+    # calculate 3Crv balance in actual pool
+    amount_base_lp = actual_pool.balances(1)
+    base_lp_total_supply = Contract(actual_pool.coins(1)).totalSupply()
+
+    # calculate USD balance in base pool assuming actual is a meta pool
+    amount1 = 0
+    for i in range(3):
+        amount1 += (base_pool.balances(i) * amount_base_lp) \
+            // base_lp_total_supply
+
+    # mint token amounts to acc prior to adding liquidity to mock pool
+    mock_token0.mint(acc, amount0, sender=acc)
+    mock_token1.mint(acc, amount1, sender=acc)
+
+    # add the minted liquidity
+    receipt = mock_pool.add_liquidity([amount0, amount1], 0, sender=acc)
+    return receipt.return_value
+
+
 def main():
     """
     Deploys a new Curve pool using mock tokens to simulate
@@ -54,12 +95,15 @@ def main():
     # mim pool to replicate
     actual_pool = mim_curve_pool()
 
+    # base 3crv pool to replicate
+    base_pool = crv3_curve_pool()
+
     # mock ERC20 tokens needed for pool
     mock_token0 = deploy_mock_token("Mock MIM", "MMIM", acc)
-    mock_token1 = deploy_mock_token("Mock DAI", "MDAI", acc)
+    mock_token1 = deploy_mock_token("Mock USD", "MUSD", acc)
     mock_lp = deploy_mock_token("Mock LP", "MLP", acc)
 
-    # mock curve pool
+    # deploy mock curve pool
     mock_pool = deploy_mock_pool(
         actual_pool,
         mock_token0,
@@ -67,3 +111,17 @@ def main():
         mock_lp,
         acc
     )
+
+    # add liquidity in proportion to mim pool
+    minted_lp_amount = mint_liquidity_to_mock_pool(
+        actual_pool,
+        base_pool,
+        mock_pool,
+        mock_token0,
+        mock_token1,
+        acc
+    )
+    assert minted_lp_amount == mock_lp.balanceOf(acc)
+
+    # TODO: check mock_pool.get_dy and do the actual swap to test slippage
+    # TODO: and marginal price changes
